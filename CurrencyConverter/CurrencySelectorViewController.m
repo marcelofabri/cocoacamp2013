f551d9ae0e53897e344f8e104c4b5448c7bd92d1
Marcelo Fabri <marcelofabrimf@gmail.com>
2013-03-10 20:25:24 -0300

    Busca!

--- CurrencyConverter/CurrencySelectorViewController.m
+++ CurrencyConverter/CurrencySelectorViewController.m
@@ -8,9 +8,11 @@
 
 #import "CurrencySelectorViewController.h"
 
-@interface CurrencySelectorViewController ()
+@interface CurrencySelectorViewController () <UISearchDisplayDelegate>
 
 @property (nonatomic, strong) NSArray *sortedCurrenciesKeys;
+@property (nonatomic, strong) NSArray *filteredCurrencies;
+
 @end
 
 @implementation CurrencySelectorViewController
@@ -54,15 +56,20 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return [self.filteredCurrencies count];
+    }
+    
     return [self.sortedCurrenciesKeys count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
-    cell.textLabel.text = self.availableCurrencies[self.sortedCurrenciesKeys[indexPath.row]];
+    NSArray *arr = tableView == self.searchDisplayController.searchResultsTableView ? self.filteredCurrencies : self.sortedCurrenciesKeys;
+    cell.textLabel.text = self.availableCurrencies[arr[indexPath.row]];
     
     return cell;
 }
@@ -70,18 +77,24 @@
 
 #pragma mark - Table view delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-    NSString *selectedCurrency = self.sortedCurrenciesKeys[indexPath.row];
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSArray *arr = tableView == self.searchDisplayController.searchResultsTableView ? self.filteredCurrencies : self.sortedCurrenciesKeys;
+    NSString *selectedCurrency = arr[indexPath.row];
     [self.delegate currencySelector:self didSelectCurrency:selectedCurrency];
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark - UISearchDisplayController Delegate Methods
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
+    // Tells the table data source to reload when text changes
+    
+    NSIndexSet *indexes = [self.sortedCurrenciesKeys indexesOfObjectsPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        return [self.availableCurrencies[obj] rangeOfString:searchString options:NSCaseInsensitiveSearch].length > 0;
+    }];
+    
+    self.filteredCurrencies = [self.sortedCurrenciesKeys objectsAtIndexes:indexes];
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
 }
 
 @end
